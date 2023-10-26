97093b70724c9a372114fa28a498a80c28820b80
JojoDom <adwoa_dadson@yahoo.com>
2023-10-26 12:40:55 +0000

    added dashboard file

---
+++ sch_management_system/lib/screens/dashboard.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class DashBoard extends StatefulWidget {
+  const DashBoard({super.key});
+
+  @override
+  State<DashBoard> createState() => _DashBoardState();
+}
+
+class _DashBoardState extends State<DashBoard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
